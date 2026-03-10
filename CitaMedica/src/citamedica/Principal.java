d68a6fb94402458d431febe7af207d33e621550f
vespertino <vespertino@ALUMNO13AULA26>
2026-03-10 20:46:28 +0100

    hola

--- CitaMedica/src/citamedica/Principal.java
+++ CitaMedica/src/citamedica/Principal.java
@@ -4,11 +4,7 @@ public class Principal {
 	
 	public static void main(String[] args) {
 
-		System.out.println("Hola soy Daniel Garcia Gestal");
-		System.out.println("Soy Marcio Candia");
-		System.out.println("Soy Jonás");
-		System.out.println("Soy Emilio");
-		System.out.println("Soy Emilio"); 
+		System.out.println("Hola soy Daniel Garcia");
 
 	}
 

394f0887d852c32773440299fdd90e67430b30ea
victoremiliopalma111 <victor.palma1@educa.madrid.org>
2026-03-11 20:16:32 +0100

    Update Principal.java

--- CitaMedica/src/citamedica/Principal.java
+++ CitaMedica/src/citamedica/Principal.java
@@ -5,11 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 
 
-		System.out.println("Hola soy Daniel Garcia");
-		System.out.println("Hola soy Daniel Garcia Gestal");
-		System.out.println("Soy Marcio Candia");
-		System.out.println("Soy Jonás");
-		System.out.println("Soy Emilio"); 
+
+
+		
 
 
 	}
